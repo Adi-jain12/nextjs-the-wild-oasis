10050544bd3e1ecd348096de029331dde95ec89b
Adi-jain12 <jaditya98@gmail.com>
2024-08-20 03:22:21 +0530

    fix bug

--- client/app/api/cabins/[cabinId]/route.js
+++ client/app/api/cabins/[cabinId]/route.js
@@ -1,18 +1,18 @@
-import { getBookedDatesByCabinId, getCabin } from '@/misc/data-service';
+// import { getBookedDatesByCabinId, getCabin } from '@/misc/data-service';
 
-export async function GET(request, { params }) {
-	const { cabinId } = params;
+// export async function GET(request, { params }) {
+// 	const { cabinId } = params;
 
-	try {
-		const [cabin, bookedDates] = await Promise.all([
-			getCabin(cabinId),
-			getBookedDatesByCabinId(cabinId),
-		]);
+// 	try {
+// 		const [cabin, bookedDates] = await Promise.all([
+// 			getCabin(cabinId),
+// 			getBookedDatesByCabinId(cabinId),
+// 		]);
 
-		return Response.json({ cabin, bookedDates });
-	} catch {
-		return Response.json({ message: 'Cabin not found' });
-	}
-}
+// 		return Response.json({ cabin, bookedDates });
+// 	} catch {
+// 		return Response.json({ message: 'Cabin not found' });
+// 	}
+// }
 
 // export async function POST() {}
